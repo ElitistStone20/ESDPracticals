52ab41ba57e7b0e8a74bc7f9a229363bb15f9b2a
Callum <callum3gill@gmail.com>
2020-10-20 11:55:29 +0100

    Add files via upload

--- Triangle.java
+++ Triangle.java
@@ -8,25 +8,25 @@ public class Triangle {
     private int[] sides = new int[3];
     private List<Integer> angles = new ArrayList<>();
     String triangleType = null;
-    public Triangle(int side1, int side2, int side3){
+    public Triangle(int side1, int side2, int side3){   //Constructor 
         sides[0] = side1;
         sides[1] = side2;
-        sides[2] = side3;
+        sides[2] = side3;    
         CalculateTriangleType();
     }
-    public String GetType(){
-        return triangleType;
+    public String GetType(){       
+        return triangleType;    //Returns the type of triangle
     }
-    private void CalculateTriangleType(){
+    private void CalculateTriangleType(){   //Determines the type of triangle
         int equalSides = 0;        
-        for (int i = 0; i < sides.length-1; i++){
+        for (int i = 0; i < sides.length-1; i++){   //Calculates the number of equal sides
             if (sides[i+1] == sides[i]) {
                 equalSides++;
             }
         }
-        switch (equalSides){                     
+        switch (equalSides){                           
             case 1:
-                triangleType = "Isosceles";
+                triangleType = "Isosceles"; 
                 break;
             case 2:
                 triangleType = "Equilateral";
--- Week1ESD.java
+++ Week1ESD.java
@@ -21,21 +21,21 @@ public class Week1ESD {
         }
     } 
     private static void CreateTriangle(){
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);    //Declare scanner
         List<Integer> sides = new ArrayList<>();
-        for (int i = 0; i < 3; i++){
+        for (int i = 0; i < 3; i++){        //Loop and get inputs
             System.out.println("Please enter a side length");
-            sides.add(sc.nextInt());
+            sides.add(sc.nextInt());    //Add input to list
         }        
-        if (CheckValidity(sides.get(0), sides.get(1), sides.get(2))){
-            Triangle triangle = new Triangle(sides.get(0), sides.get(1), sides.get(2));
-            System.out.println(triangle.GetType());
-        }
+        if (CheckValidity(sides.get(0), sides.get(1), sides.get(2))){   //Check validity of triangle
+            Triangle triangle = new Triangle(sides.get(0), sides.get(1), sides.get(2)); //If valid create new triangle instance
+            System.out.println(triangle.GetType());     //Get the type of triangle
+        }   
         else{
             System.out.println("Invalid triangle");
         }
     }
     private static boolean CheckValidity(int side1, int side2, int side3){
-        return !(side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1);
+        return !(side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1);   //Checks validity
     }
 }
